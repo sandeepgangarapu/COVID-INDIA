75005650a202b67bca4a44307f482e73a6360675
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-04-03 23:54:41 -0500

    Added MOHW numbers and push them to s3

--- daily_india_data.R
+++ daily_india_data.R
@@ -19,6 +19,45 @@ mywrite <- function(x, file) {
 today_date <- as.Date(Sys.Date(), "%y%m%d/")
 
 
+# ----------------------------------------------------------------------------------------
+# HTML Scraping of MOHFW website
+# ----------------------------------------------------------------------------------------
+mohw_url <- "https://www.mohfw.gov.in/"
+html <- read_html(mohw_url) 
+tbl_path = "//table[@class='table table-striped']"
+tbl = html_nodes(html, xpath = tbl_path)[[1]]
+df <- html_table(tbl)
+df <- df[,c(2:5)]
+colnames(df) <- c("State", "Confirmed", "Recovered", "Deceased")
+state_data_mohw <- df %>% mutate(Hospitalized = Confirmed-(Recovered+Deceased)) %>%
+  merge(state_code_data, by.x=c("State"), by.y=c("state")) %>%
+  select(-State) %>%
+  mutate(State=code) %>%
+  select(-c(code, Confirmed))
+
+write.csv(state_data_mohw,  paste("./log/state_mohw", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
+
+s3write_using(state_data_mohw, FUN = mywrite,
+              bucket = "coronadailyupdates",
+              object = "state_mohw")
+
+Confirmed <- max(df$Confirmed)
+Recovered <- max(df$Recovered)
+Deceased <- max(df$Deceased)
+Hospitalized = Confirmed-(Recovered+Deceased)
+country_data_mohw <- data.frame(Hospitalized, Recovered, Deceased)
+write.csv(country_data_mohw,  paste("./log/country_mohw", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
+
+s3write_using(country_data_mohw, FUN = mywrite,
+              bucket = "coronadailyupdates",
+              object = "country_mohw")
+
+# ----------------------------------------------------------------------------------------
+
+
+
+
+
 df <- covid_data %>% group_by(detecteddistrict, detectedstate, currentstatus) %>% summarise(cnt = n()) %>% ungroup()
 df <- df[2:dim(df)[1],]
 df <- df %>%  pivot_wider(names_from = currentstatus, values_from = cnt, values_fill=list(cnt = 0)) %>%

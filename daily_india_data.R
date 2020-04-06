00e298a4aca242405dbd0c9212c4b8362095c591
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-04-05 22:25:16 -0500

    commit before new covid api

--- daily_india_data.R
+++ daily_india_data.R
@@ -4,14 +4,17 @@ library(tidyr)
 library(lubridate)
 library(aws.s3)
 library(rvest)
-source("amazon_creds.R")
 
 
 setwd("G:\\My Drive\\Projects\\COVID_INDIA")
+source("amazon_creds.R")
 
 covid_data <- fromJSON("https://api.covid19india.org/raw_data.json")[[1]]
-pincode_data <- read.csv("pincode_data.csv") %>% drop_na() 
-state_code_data <-read.csv("state_code_data.csv")
+state_code_data <-read.csv("state_code_data.csv") %>% rename(State = state)
+
+pincode_data <- read.csv("./pincode/pincode_cleaned.csv", stringsAsFactors = TRUE) %>% drop_na() 
+write.csv(pincode_data %>% select(District) %>% unique(), "district1.csv")
+write.csv(covid_data %>% select(detecteddistrict) %>% unique(), "district2.csv")
 
 mywrite <- function(x, file) {
   write.csv(x, file, row.names=FALSE)
@@ -26,11 +29,17 @@ mohw_url <- "https://www.mohfw.gov.in/"
 html <- read_html(mohw_url) 
 tbl_path = "//table[@class='table table-striped']"
 tbl = html_nodes(html, xpath = tbl_path)[[1]]
-df <- html_table(tbl)
+df <- html_table(tbl) 
 df <- df[,c(2:5)]
-colnames(df) <- c("State", "Confirmed", "Recovered", "Deceased")
-state_data_mohw <- df %>% mutate(Hospitalized = Confirmed-(Recovered+Deceased)) %>%
-  merge(state_code_data, by.x=c("State"), by.y=c("state")) %>%
+colnames(df) <- c("State", "Confirmed", "Recovered", "Deceased")  
+
+df <- df %>%
+  mutate(Confirmed = as.numeric(Confirmed), Recovered=as.numeric(Recovered), Deceased=as.numeric(Deceased)) %>% drop_na()
+df[df$State=="Telengana","State"] <- "Telangana"
+state_data_mohw <- df %>% 
+  merge(state_code_data, by.x=c("State"), by.y=c("State")) %>%
+  mutate(Confirmed = as.numeric(Confirmed), Recovered=as.numeric(Recovered), Deceased=as.numeric(Deceased)) %>% 
+  mutate(Hospitalized = as.numeric(Confirmed)-(as.numeric(Recovered)+as.numeric(Deceased))) %>%
   select(-State) %>%
   mutate(State=code) %>%
   select(-c(code, Confirmed))
@@ -41,10 +50,10 @@ s3write_using(state_data_mohw, FUN = mywrite,
               bucket = "coronadailyupdates",
               object = "state_mohw")
 
-Confirmed <- max(df$Confirmed)
-Recovered <- max(df$Recovered)
-Deceased <- max(df$Deceased)
-Hospitalized = Confirmed-(Recovered+Deceased)
+Confirmed <- sum(df$Confirmed)
+Recovered <- sum(df$Recovered)
+Deceased <- sum(df$Deceased)
+Hospitalized = as.numeric(Confirmed)-(as.numeric(Recovered)+as.numeric(Deceased))
 country_data_mohw <- data.frame(Hospitalized, Recovered, Deceased)
 write.csv(country_data_mohw,  paste("./log/country_mohw", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
 
@@ -61,14 +70,24 @@ s3write_using(country_data_mohw, FUN = mywrite,
 df <- covid_data %>% group_by(detecteddistrict, detectedstate, currentstatus) %>% summarise(cnt = n()) %>% ungroup()
 df <- df[2:dim(df)[1],]
 df <- df %>%  pivot_wider(names_from = currentstatus, values_from = cnt, values_fill=list(cnt = 0)) %>%
-  rename(district = detecteddistrict, state = detectedstate) %>% select(state, district, Hospitalized,Deceased, Recovered)%>%
+  rename(District = detecteddistrict, State = detectedstate) %>% select(State, District, Hospitalized,Deceased, Recovered)%>%
   mutate(Date = as.Date(Sys.Date())) 
+
+# COUNTRY LEVEL COVID DATA
+
+country_level_data <- df %>% select(-c(District, Date, State)) %>% summarise_all(funs(sum))
+
+write.csv(country_level_data,  paste("./log/country", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
+
+s3write_using(country_level_data, FUN = mywrite,
+              bucket = "coronadailyupdates",
+              object = "country")
 
 # STATE LEVEL COVID DATA
 
 
-state_level_data <- df %>% select(-c(district, Date)) %>% group_by(state) %>% summarise_all(funs(sum)) %>% ungroup() %>%
-  merge(state_code_data, by.x=c("state"), by.y=c("state"))  %>% mutate(State=code) %>% select(-c(code, state))
+state_level_data <- df %>% select(-c(District, Date)) %>% group_by(State) %>% summarise_all(funs(sum)) %>% ungroup() %>%
+  merge(state_code_data, by.x=c("State"), by.y=c("State"))  %>% mutate(State=code) %>% select(-c(code))
 
 write.csv(state_level_data,  paste("./log/state", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
 
@@ -76,12 +95,47 @@ s3write_using(state_level_data, FUN = mywrite,
               bucket = "coronadailyupdates",
               object = "state")
 
+
+
 # PINCODE LEVEL COVID DATA
 
-final_data <- merge(pincode_data, df, by.x=c("Districtname"), by.y=c("district")) 
-final_data <- merge(final_data, state_code_data, by.x=c("state"), by.y=c("state")) %>% 
-select(pincode, code, Districtname, Hospitalized, Deceased, Recovered) %>% 
-  rename(Pincode = pincode, State = code, District = Districtname) 
+final_data <- merge(pincode_data, df, by.x=c("District"), by.y=c("District")) %>% rename(State=State.x)
+
+
+final_data <- merge(final_data, state_code_data, by.x=c("State"), by.y=c("State")) %>% 
+select(Pincode, code, District, Hospitalized, Deceased, Recovered) %>% 
+  rename(State = code) 
+
+
+# # ----------------------------------------------------------------------------------------
+# # Pincode debug
+# 
+# pin_sub <- unique(pincode_data$Districtname)
+# covid_sub <- unique(df$district)
+# final_sub <- unique(final_data$District)
+# 
+# 
+# diff1 <- setdiff(covid_sub, final_sub)
+# diff2 <- setdiff(pin_sub, final_sub)
+# 
+# 
+# dist.name<-adist(diff1,diff2, partial = TRUE, ignore.case = TRUE)
+# 
+# # We now take the pairs with the minimum distance
+# min.name<-apply(dist.name, 1, min)
+# 
+# match.s1.s2<-NULL  
+# for(i in 1:nrow(dist.name))
+# {
+#   s2.i<-match(min.name[i],dist.name[i,])
+#   s1.i<-i
+#   match.s1.s2<-rbind(data.frame(s2.i=s2.i,s1.i=s1.i,s2name=diff2[s2.i,], s1name=diff1[s1.i,], adist=min.name[i]),match.s1.s2)
+# }
+# # and we then can have a look at the results
+# View(match.s1.s2)
+
+
+# ----------------------------------------------------------------------------------------
 
 write.csv(final_data, paste("./log/district", today_date,"_", hour(Sys.time()), ".csv",sep=""), row.names = FALSE)
 
--- pincode/district_pincode.R
+++ pincode/district_pincode.R
@@ -1,7 +1,87 @@
 setwd("G:\\My Drive\\Projects\\COVID_INDIA\\pincode")
 
-df <- read.csv("pincode_data.csv")
-df$dist_pin3 <- substr(df$pincode,0,3) 
-df$dist_pin4 <- substr(df$pincode,0,5)
 
-df <- df %>% select(statename, Districtname) %>% unique() %>% group_by(Districtname) %>% mutate(cnt = n())
+df <- read.csv("new_pincode.csv") %>% rename(State = StateName)
+
+
+# Telangana Districts
+
+df$District[df$District=='Ananthapur'] <- Bhadradri Kothagudem
+df$District[df$District=='Ananthapur'] <- Medchal Malkajgiri
+df$District[df$District=='Ananthapur'] <- Warangal Urban
+df$District[df$District=='Ananthapur'] <- Vikarabad
+df$District[df$District=='Ananthapur'] <- Nirmal
+df$District[df$District=='Ananthapur'] <- Sangareddy
+df$District[df$District=='Ananthapur'] <- Peddapalli
+
+
+
+df$District[df$District=='Ananthapur'] <- 'Anantapur'
+df$District[df$District=='Kasargod'] <- 'Kasaragod'
+df$District[df$District=='Gurgaon'] <- 'Gurugram'
+df$District[df$District=='Kanchipuram'] <- 'Kancheepuram'
+df$District[df$District=='Bangalore'] <- 'Bengaluru'
+df$District[df$District=='Nellore'] <- 'S.P.S. Nellore'
+df$District[df$District=='Gulbarga'] <- 'Kalaburagi'
+df$District[df$District=='Ananthapur'] <- Ahmadnagar
+df$District[df$District=='Khordh'] <- 'Khordha'
+df$District[df$District=='Jhujhunu'] <- 'Jhunjhunu'
+df$District[df$District=='Ananthapur'] <- Shahid Bhagat Singh Nagar
+df$District[df$District=='Ropar'] <- 'S.A.S. Nagar'
+df$District[df$District=='Ahmedabad'] <- 'Ahmadabad'
+df$District[df$District=='Karim Nagar'] <- 'Karimnagar'
+df$District[df$District=='Chikkaballapur'] <- 'Chikkaballapura'
+df$District[df$District=='Gandhi Nagar'] <- 'Gandhinagar'
+df$District[df$District=='Mysore'] <- 'Mysuru'
+df$District[df$District=='Ananthapur'] <- Palwal
+df$District[df$District=='Ananthapur'] <- Tiruppur
+df$District[df$District=='K.V.Rangareddy'] <- 'Ranga Reddy'
+df$District[df$District=='Bandipur'] <- 'Bandipore'
+df$District[df$District=='Bilaspur(CGH)'] <- 'Bilaspur'
+df$District[df$District=='Bagpat'] <- 'Baghpat'
+df$District[df$District=='Rajauri'] <- 'Rajouri'
+df$District[df$District=='North And Middle Andaman'] <- 'North and Middle Andaman'
+df$District[df$District=='Gondia'] <- 'Gondiya'
+df$District[df$District=='Tumkur'] <- 'Tumakuru'
+df$District[df$District=='Ananthapur'] <- Shamli
+df$District[df$District=='Davangere'] <- 'Davanagere'
+df$District[df$District=='Ananthapur'] <- Chengalpattu
+df$District[df$District=='Medinipur'] <- 'Medinipur East'
+df$District[df$District=='Ananthapur'] <- Kalimpong
+df$District[df$District=='Budgam'] <- 'Badgam'
+df$District[df$District=='Ananthapur'] <- Baramula
+df$District[df$District=='Ananthapur'] <- Buldana
+df$District[df$District=='Ananthapur'] <- Gir Somnath
+df$District[df$District=='Bellary'] <- 'Ballari'
+df$District[df$District=='Villupuram'] <- 'Viluppuram'
+df$District[df$District=='Kanyakumari'] <- 'Kanniyakumari'
+df$District[df$District=='Tuticorin'] <- 'Thoothukkudi'
+df$District[df$District=='Pondicherry'] <- 'Puducherry'
+df$District[df$District=='Cuddapah'] <- 'Y.S.R.'
+df$District[df$District=='Kamrup'] <- 'Kamrup Metropolitan'
+df$District[df$District=='Ananthapur'] <- Tirupathur
+df$District[df$District=='Tiruvarur'] <- 'Thiruvarur'
+df$District[df$District=='Dholpur'] <- 'Dhaulpur'
+df$District[df$District=='Ananthapur'] <- Ranipet
+df$District[df$District=='Tiruvallur'] <- 'Thiruvallur'
+df$District[df$District=='Hazaribag'] <- 'Hazaribagh'
+df$District[df$District=='Bagalkot'] <- 'Bagalkote'
+df$District[df$District=='Ananthapur'] <- Hapur
+df$District[df$District=='Ananthapur'] <- Nuh
+df$District[df$District=='Ananthapur'] <- South Salmara Mancachar
+df$District[df$District=='Ananthapur'] <- Kashmir
+df$District[df$District=='Belgaum'] <- 'Belagavi'
+df$District[df$District=='Marigaon'] <- 'Morigaon'
+df$District[df$District=='Bangalore Rural'] <- 'Bengaluru Rural'
+df$District[df$District=='Ananthapur'] <- Kallakurichi
+df$District[df$District=='Hathras'] <- 'Hatras'
+df$District[df$District=='Udaipur'] <- 'Chota Udaipur'
+df$District[df$District=='Nilgiris'] <- 'The Nilgiris'
+df$District[df$District=='Raebareli'] <- 'Rae Bareli'
+df$District[df$District=='Barabanki'] <- 'Bara Banki'
+
+df$District[df$District=='Ananthapur'] <- Morbi
+df$District[df$District=='Ananthapur'] <- Bhuj
+
+# 
+write.csv(df, "pincode_cleaned.csv", row.names = FALSE)
